0d6bbc421f830a49298d70ea8115dbebcf2d973f
Ankit Parkhi <ankitvilas.parkhi.cse23@itbhu.ac.in>
2025-04-10 01:25:11 +0530

    Initial commit with Tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="bg-red-500 text-white p-4 text-xl">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
